b870579ccb224a8b3034ee2d73d7d55d69cb3bd5
yuinchirn <l.serah.yuinch@gmail.com>
2018-12-07 00:45:43 +0900

    お掃除 & Scenario: test_Top画面はTODO一覧画面() の実装

--- todoApp/todoAppUITests/features/TodoListFeature.swift
+++ todoApp/todoAppUITests/features/TodoListFeature.swift
@@ -1,30 +1,10 @@
-//
-//  todoAppUITests.swift
-//  todoAppUITests
-//
-//  Created by Yuta Chiba on 2018/12/04.
-//  Copyright © 2018 xxxx. All rights reserved.
-//
-
 import XCTest
 
 class TodoListFeature: XCTestCase, TodoListSteps {
 
-    override func setUp() {
-        continueAfterFailure = false
-        XCUIApplication().launch()
-    }
-    
-    func test_TODO一覧の表示() {
-        when_アプリが起動する時()
+    func test_Top画面はTODO一覧画面() {
+        when_アプリが起動すると()
         then_TODO一覧が表示される()
     }
     
-    func test_＋ボタン押下でTODO追加画面に遷移() {
-        given_TODO一覧画面を開いている()
-        when_＋ボタンを押す時()
-        then_TODO追加画面が表示される()
-    }
-
-
 }
--- todoApp/todoAppUITests/steps/TodoListSteps.swift
+++ todoApp/todoAppUITests/steps/TodoListSteps.swift
@@ -6,48 +6,26 @@
 //  Copyright © 2018 xxxx. All rights reserved.
 //
 
-import Foundation
 import XCTest
 
 protocol TodoListSteps {
     
-    // given
-    func given_TODO一覧画面を開いている()
-    
     // when
-    func when_アプリが起動する時()
-    func when_＋ボタンを押す時()
-    
+    func when_アプリが起動すると()
+
     // then
     func then_TODO一覧が表示される()
-    func then_TODO追加画面が表示される()
 }
 
 extension TodoListSteps {
     
     // given
-    func when_アプリが起動する時() {
+    func when_アプリが起動すると() {
         XCUIApplication().launch()
-    }
-    
-    func given_TODO一覧画面を開いている() {
-        XCUIApplication().launch()
-    }
-    
-    // when
-    func when_＋ボタンを押す時() {
-        XCUIApplication().buttons["Add"].tap()
     }
     
     // then
     func then_TODO一覧が表示される() {
-        XCTAssertTrue(XCUIApplication().navigationBars["TODO"].exists)
+       XCTAssertTrue(XCUIApplication().navigationBars["TODO一覧"].exists)
     }
-    
-    func then_TODO追加画面が表示される() {
-        XCTAssertTrue(XCUIApplication().navigationBars["ADD TODO"].exists)
-    }
-    
-    
-
 }
